d6e46e5e5758261f14206df4216af55a5c6a4419
sambennion <bennionsamuel@gmail.com>
2022-03-31 13:20:30 -0600

    B&B is half working. Easy and medium

--- TSPSolver.py
+++ TSPSolver.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import queue
 from which_pyqt import PYQT_VER
 if PYQT_VER == 'PYQT5':
 	from PyQt5.QtCore import QLineF, QPointF
@@ -100,7 +101,7 @@ class TSPSolver:
 		# Set cities to shallow copy of getCities(). 
 		# Copy because we'll be popping them off in my algorithm 
 		# so we don't affect the original cities object.
-		cities = copy.copy(self._scenario.getCities())
+		cities = copy.deepcopy(self._scenario.getCities())
 		foundTour = False
 		count = 0
 		bssf = None
@@ -114,12 +115,13 @@ class TSPSolver:
 				currentCity = self.short_edge(currentCity, cities)
 				route.append(currentCity)
 			bssf = TSPSolution(route)
+			# print(route)
 			# bssf in the greedy sense will just be the first one that 
 			# find a tour.
 			if(bssf.cost < math.inf):
 				foundTour = True
 			else:
-				cities = copy.copy(self._scenario.getCities())
+				cities = copy.deepcopy(self._scenario.getCities())
 			count += 1
 		end_time = time.time()
 		results = {}
@@ -150,23 +152,29 @@ class TSPSolver:
 		# print(min_of_rows)
 		#reduce rows first
 		for i in range(n):
-			matrix[i:] -= min_of_rows[i]
+			if(min_of_rows[i] < np.inf):
+				matrix[i:] -= min_of_rows[i]
+			else:
+				min_of_rows[i] = 0 #This is for summing at the end.
 		matrix[matrix<0] = 0
 		# reduce each column
 		min_of_columns = np.amin(matrix, axis=0)
 		# print(np.shape(min_of_columns))
 		# print(matrix)
-		print(min_of_columns)
+		# print(min_of_columns)
 		for i in range(n):
-			matrix[:,i] = matrix[:,i] - min_of_columns[0, i]
+			if(min_of_columns[0, i] < np.inf):
+				matrix[:,i] = matrix[:,i] - min_of_columns[0, i]
+			else:
+				min_of_columns[0, i] = 0
 		matrix[matrix<0] = 0
 		cost = np.sum(min_of_rows) + np.sum(min_of_columns)
-		
+		# print(cost)
 		return matrix, cost
 
 
 	def branchAndBound( self, time_allowance=60.0 ):
-		cities = self._scenario.getCities()
+		cities = copy.deepcopy(self._scenario.getCities())
 		n = len(cities)
 		#Matrix uses O(n^2 space)
 		matrix = np.matrix(np.ones((n,n)) * np.inf)
@@ -177,13 +185,100 @@ class TSPSolver:
 		#Running a greedy first thing off the bat will create a baseline for upperbound.
 		#It tells us a few things that will save us time, it gives us a starting node that has
 		#a solution, and it will allow us to prune out routes that aren't fruitful early on.
-
-		greedy_results = self.greedy(time_allowance=60.0)
-		upper_bound = greedy_results['cost']
-		starting_node = greedy_results['count'] - 1
-		reduced_matrix, cost = self.reduce_matrix(copy.deepcopy(matrix))
 		
-		return
+		results = self.greedy(time_allowance=60.0)
+		state_count = n
+		pruned = 0
+		lower_bound = results['cost']
+		starting_node = results['count'] - 1
+		bssf = results['soln']
+		#Runs in n^2 time
+		start_time = time.time()
+		reduced_matrix, initial_cost = self.reduce_matrix(copy.deepcopy(matrix))
+		# currentCity = cities[starting_node]
+
+		# reduced_matrix[:,0] += np.inf
+		# print(reduced_matrix)
+		pq = []
+		#Somehow need to keep track of parent cost, 
+		# added matrix cost, and the reducucing cost.
+		
+		#Initial loop to add nodes and costs to pq. Runs O(n) time
+		for i in range(len(cities)):
+			if start_time - time_allowance <= 0:
+					break
+			edge_cost = reduced_matrix[starting_node, i]
+			if edge_cost == np.inf:
+				continue
+			new_reduced = copy.deepcopy(reduced_matrix)
+			new_reduced[starting_node] += np.inf
+			new_reduced[:,i] += np.inf
+			new_reduced[i, starting_node] = np.inf
+			new_reduced, reduction_cost = self.reduce_matrix(copy.deepcopy(new_reduced))
+			# print(new_reduced)
+			node_cost = edge_cost + initial_cost + reduction_cost
+			
+			if node_cost >= lower_bound:
+				
+				# print("Node: " + str(node_cost) + " Upper: " + str(lower_bound))
+				pruned += 1
+			else:
+				# print(i)
+				heapq.heappush(pq, (node_cost, i, copy.copy(new_reduced), [starting_node, i]))
+				# print(len(pq))
+		# print(pq[2])
+		while len(pq) > 0 and start_time - time_allowance > 0:
+			initial_cost, index, reduced_matrix, path = heapq.heappop(pq)
+			if initial_cost >= lower_bound:
+				pruned += 1
+				continue
+			for i in range(len(cities)):
+				
+				if start_time - time_allowance <= 0:
+					break
+				#We don't want to cycle back to the starting node
+				if i == starting_node:
+					continue
+				edge_cost = reduced_matrix[index, i]
+				if edge_cost == np.inf:
+					continue
+				#same as above
+				new_reduced = copy.deepcopy(reduced_matrix)
+				new_reduced[index] += np.inf
+				new_reduced[:,i] += np.inf
+				new_reduced[i, index] = np.inf
+				new_reduced, reduction_cost = self.reduce_matrix(copy.copy(new_reduced))
+				#Same
+				node_cost = edge_cost + initial_cost + reduction_cost
+				if node_cost >= lower_bound:
+					pruned += 1
+				else:
+					path.append(i)
+					if len(path) < n:
+						heapq.heappush(pq, (node_cost, i, copy.copy(new_reduced), path))
+					else:
+						#create new lower bound bssf
+						lower_bound = node_cost
+						# print(path)
+						# print(matrix)
+						# route = [cities[j] for j in path]
+						bssf = TSPSolution([cities[j] for j in path])
+						# print(bssf.route)
+
+
+		end_time = time.time()
+		results['cost'] = lower_bound
+		results['time'] = end_time - start_time
+		results['soln'] = bssf
+		results['max'] = None
+		results['total'] = None
+		results['pruned'] = pruned
+
+			# else:
+				# new_reduced[currentCity] = 
+
+		
+		return results
 
 
 
